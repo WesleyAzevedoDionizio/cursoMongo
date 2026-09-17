ad4098546a747025b5934677c04941458bedbc9e
Wesley <wesley.azevedodionizio@gmail.com>
2026-09-17 19:34:40 -0400

    returning User by id

---
+++ src/main/java/com/wesley/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.wesley.workshopmongo.resources.exception;
+
+import com.wesley.workshopmongo.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandarError err = new StandarError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
--- src/main/java/com/wesley/workshopmongo/services/UserService.java
+++ src/main/java/com/wesley/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.wesley.workshopmongo.services;
 
 import com.wesley.workshopmongo.domain.User;
 import com.wesley.workshopmongo.repository.UserRepository;
+import com.wesley.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,14 @@ public class UserService {
 
     public List<User> findAll (){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        User user = repo.findOne(id);
+        if (user == null){
+            throw new ObjectNotFoundException("User not found");
+        }
+        return user;
     }
 
 }
