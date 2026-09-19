e01f8969d33d34fd92da2d0b73bfcbc7677ba976
Wesley <wesley.azevedodionizio@gmail.com>
2026-09-19 00:31:55 -0400

    standard consutation with query methods

--- src/main/java/com/wesley/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wesley/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.wesley.workshopmongo.resources;
 
 import com.wesley.workshopmongo.domain.Post;
+import com.wesley.workshopmongo.resources.util.URL;
 import com.wesley.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,6 +20,12 @@ public class PostResource {
     public ResponseEntity<Post>  findByid(@PathVariable String id){
         Post p1 = postService.findById(id);
         return ResponseEntity.ok().body(p1);
+    }
+
+    @GetMapping(value = "/titleSearch")
+    public ResponseEntity<List<Post>>  findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        return ResponseEntity.ok().body(postService.findByTitle(text));
     }
 
 }
